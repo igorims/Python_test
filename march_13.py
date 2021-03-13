2f71c313815dc835f7365ec30d856e4a4222ae3a
igorims <fatalhope@gmail.com>
2021-03-13 12:12:17 +0300

    Initial commit

---
+++ march_13.py
@@ -0,0 +1,11 @@
+"""MARCH 13, 2021"""
+
+# Virtual environment (виртуальное окружение) / vent / env
+# python -m venv env
+# pip freeze > requirements.txt - заполнить файл всеми библиотеками, необходимыми для проекта (но лучше вручную
+# написать, скопировав в консоле вывод от pip freeze
+# pip install -r requirements.txt - установить все из списка в файле
+
+open git Bash (in the folder you need)
+git init
+git status
