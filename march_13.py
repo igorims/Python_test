d621d03a0f906e8386ade13edcf65f1252cf6e5b
Igor <fatalhope@gmail.com>
2021-03-13 12:38:43 +0300

    Added branch comment

--- march_13.py
+++ march_13.py
@@ -6,19 +6,6 @@
 # написать, скопировав в консоле вывод от pip freeze
 # pip install -r requirements.txt - установить все из списка в файле
 
-open git Bash (in the folder you need)
-git init
-git status
-git add <filename>
-git commit -m "Initial commit"
-git log (история изменений репозитория)
-git config --global
+#see info.txt for info
 
-git config --global user.name #check name
-or
-git config --global user.name "Igor" #create name
-git config --global user.email ("aa@bb.email.com") #create email
-
-    after you change file, you need to: git add march_13.py again and commit againgit l
-git checkout master #most recent
-git diff #to check difference from previous commit
+#added new branch
